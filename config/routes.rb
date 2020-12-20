a4c5dc19ecbb9cd6709df5d49b4e4c3aacf5a557
sbsamboat9 <sbsamboat9@icloud.com>
2020-12-20 07:53:52 -0500

    adding changes to config file

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
   resources :reviews
   resources :movies do
    	resources :reviews, except: [:show, :index]
+    
+      # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+    root to: "movies#index"
    end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  root to: "movies#index"
-
-end
